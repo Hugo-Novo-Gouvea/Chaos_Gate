f3db3952060dd88d18770f4f900cb474cc4a3bd8
Hugo-Novo-Gouvea <novogouvea@gmail.com>
2023-05-21 17:04:52 -0300

    Rotacao

--- Assets/Scripts/player/playerMoviment.cs
+++ Assets/Scripts/player/playerMoviment.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class playerMoviment : MonoBehaviour
 {
 
+    private Camera mainCam;
+    public Vector3 mousePos;
+    
     Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
 
     
     void Update()
     {
+        // Player Moviment
+        
         rb.velocity = new Vector2(0, 0);
 
         if (Input.GetKey(KeyCode.W))
@@ -33,7 +39,17 @@ public class playerMoviment : MonoBehaviour
         {
             rb.velocity = new Vector2(3, rb.velocity.y);
         }
-    }
 
+        // Player Rotation
+
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 rotation = mousePos - transform.position;
+
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0, 0, rotZ);
+
+    }
 
 }
